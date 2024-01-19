3fd8c7c1d5df87b8a08a1372e7aef8a47d2e7f5b
soongu <naong526@naver.com>
2024-01-19 09:54:03 +0900

    #6. 회원엔터티, 레파지토리, 서비스 회원가입 기능 생성

--- src/main/java/com/study/todoapi/todo/controller/TodoController.java
+++ src/main/java/com/study/todoapi/todo/controller/TodoController.java
@@ -22,6 +22,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/todos")
+@CrossOrigin(origins = {"http://localhost:3000"}) // API접근을 허용할 클라이언트 아이피
 public class TodoController {
 
     private final TodoService todoService;
